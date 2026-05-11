a4ad7d26a2cf60f842d339cd72bdce1e8d804120
jkakemi <julia.koba@ufu.br>
2026-05-10 22:27:28 -0300

    implement competitive core with codeforces sync, persistence and endpoints

---
+++ competitive-core/src/main/java/br/com/ufu/recsys/competitivecore/CompetitiveCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufu.recsys.competitivecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitiveCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitiveCoreApplication.class, args);
+    }
+
+}
---
+++ competitive-core/src/test/java/br/com/ufu/recsys/competitivecore/CompetitiveCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufu.recsys.competitivecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitiveCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
